12ce2ea8f3fdcbd44592ea86cb6a27622b6d31b4
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-11-15 17:01:35 +0400

    Book Controller added to the system.

---
+++ DemoApplication/DemoApplication/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DemoApplication.Database;
+using DemoApplication.Database.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApplication.Controllers
+{
+    public class BookController : Controller
+    {
+        //Read
+        public List<Book> Index()
+        {
+            return DatabaseAccess.Books;
+        }
+
+        [HttpPost]
+        //Create 
+        public ViewResult Add(int id, [FromForm] string name, [FromForm] decimal price)
+        {
+            Database.DatabaseAccess.Books.Add(new Book(id, name, price)); //FULLY QUALIFIED NAMESPACE
+            return View();
+        }
+    }
+}
