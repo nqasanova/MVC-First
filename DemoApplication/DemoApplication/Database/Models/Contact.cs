388177b549180c60b3073d1e3de27a18b977c5d3
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-11-15 20:34:20 +0400

    Contact class added to the system.

---
+++ DemoApplication/DemoApplication/Database/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace DemoApplication.Database.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Message { get; set; }
+
+        public Contact(int id, string name, string email, string phone, string message)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Phone = phone;
+            Message = message;
+        }
+    }
+}
