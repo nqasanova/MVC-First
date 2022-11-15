47f9c331d2d755f42a3b4102ffb1b253761d05f1
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-11-15 20:41:51 +0400

    Made modifications to Database.

--- DemoApplication/DemoApplication/Database/Database.cs
+++ DemoApplication/DemoApplication/Database/Database.cs
@@ -6,5 +6,16 @@ namespace DemoApplication.Database
     public class DatabaseAccess
     {
         public static List<Book> Books { get; set; } = new List<Book>();
+        public static List<Contact> Contacts { get; set; } = new List<Contact>(); //Strong retype Prinsipi
+    }
+
+    public class TablePKAutoIncrement
+    {
+        private static int contactCounter;
+
+        public static int ContactCounter //default dəyər sıfırdır. 1-dən oxumağa başlayacaq
+        {
+            get { return contactCounter++; }
+        }
     }
 }
