f5d3c0c208b55473023757f5b62b1112b5bc2842
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-11-17 17:02:42 +0400

    Authentication Controller modified.

--- DemoApplication/DemoApplication/Controllers/AuthenticationController.cs
+++ DemoApplication/DemoApplication/Controllers/AuthenticationController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using DemoApplication.Database;
+using DemoApplication.Database.ViewModels.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemoApplication.Controllers
@@ -10,8 +12,28 @@ namespace DemoApplication.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Register(RegisterViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            Database.DatabaseAccess.Users.Add(new Database.Models.User
+            {
+                Id = TablePkAutoincrement.UserCounter,
+                Email = viewModel.Email,
+                Password = viewModel.Password
+            });
+            //add to db
+
             return View();
         }
     }
