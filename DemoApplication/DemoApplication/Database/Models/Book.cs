21f607dd112c4296015eddc401ce8482370ff1ae
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-11-17 17:05:24 +0400

    Book Model modified and added to the system.

--- DemoApplication/DemoApplication/Database/Models/Book.cs
+++ DemoApplication/DemoApplication/Database/Models/Book.cs
@@ -4,14 +4,9 @@ namespace DemoApplication.Database.Models
     public class Book
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
         public decimal Price { get; set; }
-
-        public Book(int id, string name, decimal price)
-        {
-            Id = id;
-            Name = name;
-            Price = price;
-        }
+        public DateTime CreatedAt { get; set; }
     }
 }
